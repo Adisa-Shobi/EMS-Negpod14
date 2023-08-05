f9371bfb729f7ed77fa6aa5e3b1f9737ab3b8ac2
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-08-05 09:43:23 +0000

    Create: skeletal version of project created, tasks assigned

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Program entry point
+"""
+
+# TODO: @Kuir Define a function view_contestants that handle when the user picks option 1
+# TODO: @Mugisha Define a function register_contestant to handle when the user picks option 2
+# TODO: @Shobi Define a function start_session that handles when the user picks option 3
+
+def main():
+    '''
+    Starts program flow
+    '''
+    # TODO: @Eddy write code to print the options the user can chose from on start of the applicaion
+    # Please pick an option
+    # 1). View contestants   2). Register contestant   3). Start voting session
+
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ models/contestant.py
@@ -0,0 +1,17 @@
+"""
+Defines contestant class which manages each contestant and their votes
+"""
+
+class Contestant:
+    '''
+    Represents a single contestant
+    '''
+    # TODO: @favor define private instance varaibles to store description, name, D.O.B, Origin
+    # TODO: @Mahamat define a function print_summary that prints a summarized version of contestant information
+    # —----------------------------
+    #|          Contestant 1      |
+    #|     Name: Tinubu           |
+    #|                            |
+    #|                            |
+    #|                            |
+    # —----------------------------
