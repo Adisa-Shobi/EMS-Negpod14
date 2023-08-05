ed26b7de2cc8470af20f95f874544fdfe846489f
GChukwudi <g.chukwudi@alustudent.com>
2023-08-05 05:27:39 -0700

    private instance set

--- models/contestant.py
+++ models/contestant.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Defines contestant class which manages each contestant and their votes
 """
@@ -6,7 +7,17 @@ class Contestant:
     '''
     Represents a single contestant
     '''
-    # TODO: @favor define private instance varaibles to store description, name, D.O.B, Origin
+    def __init__(self):
+        self.__name = ""
+        self.__origin = ""
+        self.__Biography = ""
+        self.__DOB = ""
+    def set_contestant_info(self, name, origin, Biography, DOB):
+        self.__name = name
+        self.__origin = origin
+        self.__Biography = Biography
+        self.__DOB = DOB
+
     # TODO: @Mahamat define a function print_summary that prints a summarized version of contestant information
     # —----------------------------
     #|          Contestant 1      |
