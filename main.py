1b2c928796f138a97dce296d448b113b77d1c97c
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-08-13 19:58:10 +0000

    Update: contestant object used

--- main.py
+++ main.py
@@ -2,11 +2,17 @@
 """
 Program entry point
 """
+from mailbox import Message
+import os
+from models.contestant import Contestant
+from constants import welcome_message
+
 
 # TODO: @Kuir Define a function view_contestants that handle when the user picks option 1
 # TODO: @Mugisha Define a function register_contestant to handle when the user picks option 2
 # TODO: @Shobi Define a function start_session that handles when the user picks option 3
 
+contestants = []
 def main():
     '''
     Starts program flow
@@ -14,23 +20,40 @@ def main():
     # TODO: @Eddy write code to print the options the user can chose from on start of the applicaion
     # Please pick an option
     # 1). View contestants   2). Register contestant   3). Start voting session
+    running = True
+    while running:
+        print(welcome_message)
+        print("Press the following numbers that corresponds to with your choice.")
+        os.system('clear')
+        choice = input(
+            "\
+            Press '1' to view contestants \n\
+            Press '2' to register contestants \n\
+            Press '3' to start a session\n\
+            Press '4' to quit.\n")
+        if choice == "1":
+            view_contestant()
+        elif choice == "2":
+            register_contestant()
+        elif choice == "3":
+            start_session()
+        elif choice == "4":
+            print("Program succesfully closed")
+            running = False
+        else :
+            print(choice)
+            print("Wrong input. Try again!")
+            pass
 
-    print("Welcome to our system. \nPress the following numbers that corresponds to with your choice.")
-    choice = input("Press '1' to view contestants \nPress '2' to register contestants \nPress '3' to start a session ")
-    if choice == 1:
-        view_contestant()
-    elif choice == 2:
-        register_contestant()
-    elif choice == 3:
-        start_session()
-    else :
-        print("Wrong input. Try again!")
         pass
 
-    pass
+def start_session():
+    '''
+    Starts voting session based on currently added contestants
+    '''
+    os.system('clear')
 
-if __name__ == "__main__":
-    main()
+
 
 #Mugisha's task: Register contestant function
 # Function to register a contestant
@@ -41,14 +64,12 @@ def register_contestant():
     dob = input("D.O.B: ")
     link = input("Bio link (optional): ")
     description = input("Write a short description: ")
+    password = input("Please enter a password: ")
 
-    contestant = {
-        "name": name,
-        "origin": origin,
-        "dob": dob,
-        "link": link,
-        "description": description
-    }
+    contestant = Contestant(name=name, origin=origin, DOB=dob, password=password, link=link)
 
     contestants.append(contestant)
     print("Contestant registered!")
+
+if __name__ == "__main__":
+    main()
--- models/contestant.py
+++ models/contestant.py
@@ -7,22 +7,23 @@ class Contestant:
     '''
     Represents a single contestant
     '''
-     def __init__(self, name="", origin="", Biography="", DOB=""):
-     """
-       Initializes a new Contestant object.
-
+    def __init__(self, name="", origin="", description="", DOB="", password="", link=""):
+        """
+        Initializes a new Contestant object.
        :param name: The name of the contestant
        :param origin: The origin of the contestant
        :param Biography: The biography of the contestant
        :param DOB: The date of birth of the contestant
        """
-       self.__name = name
-       self.__origin = origin
-       self.__Biography = Biography
-       self.__DOB = DOB
+        self.__name = name
+        self.__origin = origin
+        self.__description = description
+        self.__password = password
+        self.__link = link
+        self.__DOB = DOB
 
         # A function print_summary that prints a summarized version of contestant information
-     def print_summary(self):
+    def print_summary(self):
         # Define the border string
         border = " —" + "-" * (len(self.__name) + 18) + "— "
 
