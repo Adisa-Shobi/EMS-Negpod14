572819dca94e4bd5920706b4735be63378d34ccb
GChukwudi <g.chukwudi@alustudent.com>
2023-08-13 11:49:41 -0700

    constestant.py updated

--- models/contestant.py
+++ models/contestant.py
@@ -7,37 +7,32 @@ class Contestant:
     '''
     Represents a single contestant
     '''
-    def __init__(self):
-        self.__name = ""
-        self.__origin = ""
-        self.__Biography = ""
-        self.__DOB = ""
-    def set_contestant_info(self, name, origin, Biography, DOB):
-        self.__name = name
-        self.__origin = origin
-        self.__Biography = Biography
-        self.__DOB = DOB
-# A function print_summary that prints a summarized version of contestant information
-    def print_summary(self):
+     def __init__(self, name="", origin="", Biography="", DOB=""):
+     """
+       Initializes a new Contestant object.
+
+       :param name: The name of the contestant
+       :param origin: The origin of the contestant
+       :param Biography: The biography of the contestant
+       :param DOB: The date of birth of the contestant
+       """
+       self.__name = name
+       self.__origin = origin
+       self.__Biography = Biography
+       self.__DOB = DOB
+
+        # A function print_summary that prints a summarized version of contestant information
+     def print_summary(self):
         # Define the border string
         border = " —" + "-" * (len(self.__name) + 18) + "— "
 
         #print the contestant summary
         
         print(border)
-        print("|" + f"         {self.__name}" + "           |")
-        print("|" + "                      " + "           |")
-        print("|" + f" Name: {self.__name}" + "           |")
-        print("|" + f" Origin: {self.__origin}" + "         |")
-        print("|" + f" Bio: {self.__Biography}" + "         |")
-        print("|" + f" DOB: {self.__DOB}" + "     |")
+        print("|" + f"  {self.__name}" + "           |")
+        print("|" + "                " + "           |")
+        print("|" + f" Name: {self.__name}" + "      |")
+        print("|" + f" Origin: {self.__origin}" + "  |")
+        print("|" + f" Bio: {self.__Biography}" + "  |")
+        print("|" + f" DOB: {self.__DOB}" + "        |")
         print(border)
-
-
-    # —----------------------------
-    #|          Contestant 1      |
-    #|     Name: Tinubu           |
-    #|                            |
-    #|                            |
-    #|                            |
-    # —----------------------------
